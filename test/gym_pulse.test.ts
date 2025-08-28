efd712c5daae7043edd4958ac8e2f5e97ce33615
Hercules03 <herculeskyl03628@gmail.com>
2025-08-29 00:40:48 +0800

    Initial commit

---
+++ test/gym_pulse.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as GymPulse from '../lib/gym_pulse-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/gym_pulse-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new GymPulse.GymPulseStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
